116a897481ea3234e75c684127afcbb264aebeba
Marouan Boulli <Marouan.boulli@grenoble-inp.org>
2024-01-18 16:40:18 +0100

    move mqtt initialization to the top

--- app.py
+++ app.py
@@ -65,6 +65,21 @@ app = Flask(__name__)
 # https://www.fullstackpython.com/flask-globals-session-examples.html
 # https://stackoverflow.com/questions/49664010/using-variables-across-flask-routes
 app.secret_key = 'BAD_SECRET_KEY'
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
+# Initialisation MQTT
+app.config['MQTT_BROKER_URL'] =  "test.mosquitto.org"
+app.config['MQTT_BROKER_PORT'] = 1883
+#app.config['MQTT_USERNAME'] = ''  # Set this item when you need to verify username and password
+#app.config['MQTT_PASSWORD'] = ''  # Set this item when you need to verify username and password
+#app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
+app.config['MQTT_TLS_ENABLED'] = False  # If your broker supports TLS, set it True
+
+topicname = "uca/iot/piscine"
+topicname_second = "uca/iot/tajine_granted"
+mqtt_client = Mqtt(app)
+
+
   
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
 @app.route('/')
@@ -154,19 +169,6 @@ def publish_message():
    request_data = request.get_json()
    publish_result = mqtt_client.publish(request_data['topic'], request_data['msg'])
    return jsonify({'code': publish_result[0]})
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
-# Initialisation MQTT
-app.config['MQTT_BROKER_URL'] =  "test.mosquitto.org"
-app.config['MQTT_BROKER_PORT'] = 1883
-#app.config['MQTT_USERNAME'] = ''  # Set this item when you need to verify username and password
-#app.config['MQTT_PASSWORD'] = ''  # Set this item when you need to verify username and password
-#app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
-app.config['MQTT_TLS_ENABLED'] = False  # If your broker supports TLS, set it True
-
-topicname = "uca/iot/piscine"
-topicname_second = "uca/iot/tajine_granted"
-mqtt_client = Mqtt(app)
 
 @mqtt_client.on_connect()
 def handle_connect(client, userdata, flags, rc):
